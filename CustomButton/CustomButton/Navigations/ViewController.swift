73038073e6fcff850c50cbced5e191142d8e0626
neonegar <negar@neocore.tech>
2024-07-30 19:49:10 +0330

    create project
    - set ViewController as rootViewController
    - grouping files

---
+++ CustomButton/CustomButton/Navigations/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  CustomButton
+//
+//  Created by Negar Moshtaghi on 7/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
